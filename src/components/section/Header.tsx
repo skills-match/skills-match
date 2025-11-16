f6c45ccbbe1b2fdbdeeb459923a1dcc0c2d657b7
iagoliziero <iagolizierox14@gmail.com>
2025-11-16 12:17:43 -0300

    fix: refactor Header component for improved navigation and styling consistency

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -18,15 +18,14 @@ const Header: React.FC = () => {
 
   const [isLoggedIn] = useState<string>(localStorage.getItem("loggedIn"));
 
-  
   const navigateProfile = (): void => {
     const userId: string | null = localStorage.getItem("userId");
-     if(!userId) {
-      alert("User not found")
+    if (!userId) {
+      alert("User not found");
       return;
-     }
-     navigate(`/perfil/${userId}`);
-  }
+    }
+    navigate(`/perfil/${userId}`);
+  };
 
   const navigate = useNavigate();
 
@@ -47,7 +46,7 @@ const Header: React.FC = () => {
           </div>
 
           {/* Desktop Navigation */}
-          <nav className="hidden xl:flex space-x-1">
+          <nav className="hidden xl:flex space-x-2 items-center">
             {navigationItems.map(
               (item) =>
                 item.name !== "Entrar" &&
@@ -58,82 +57,83 @@ const Header: React.FC = () => {
                       setIsMobileMenuOpen(false);
                       navigate(item.path);
                     }}
-                    className={`block px-4 py-3 rounded-lg text-base font-medium transition-colors ${
+                    className={`block px-2 py-3 rounded-lg text-base font-medium transition-colors ${
                       location.pathname === item.path
-                        ? "text-foreground hover:text-primary font-semibold"
-                        : "text-foreground hover:text-primary font-semibold"
+                        ? "text-foreground text-sm hover:text-primary font-semibold"
+                        : "text-foreground text-sm hover:text-primary font-semibold"
                     }`}
                   >
                     {item.name}
                   </button>
                 )
             )}
+
+            {isLoggedIn !== "true" ? (
+              <div className="flex gap-3">
+                <Button
+                  onClick={() => navigate("/login")}
+                  size="sm"
+                  className="hidden xl:flex gap-2 items-center justify-center "
+                  variant="outline"
+                >
+                  {" "}
+                  <i>
+                    <LogIn size={18} />{" "}
+                  </i>{" "}
+                  Entrar{" "}
+                </Button>
+                <Button
+                  onClick={() => navigate("/registrar")}
+                  size="sm"
+                  className="hidden xl:flex gap-2 items-center text-white justify-center"
+                  variant="primary"
+                >
+                  {" "}
+                  <i>
+                    <UserRoundPlus size={18} className="text-white" />{" "}
+                  </i>{" "}
+                  Criar Conta{" "}
+                </Button>
+              </div>
+            ) : (
+              <div className="flex gap-3">
+                <Button
+                  size="sm"
+                  onClick={() => {
+                    navigate("/home");
+                    localStorage.setItem("loggedIn", "false");
+                    window.location.reload();
+                  }}
+                  variant="outline"
+                  className="hidden xl:flex gap-2 items-center"
+                >
+                  <i>
+                    {" "}
+                    <LogOut size={18} />{" "}
+                  </i>
+                  <p className="font-medium "> Sair</p>
+                </Button>
+                <Button
+                  size="sm"
+                  onClick={() => navigateProfile()}
+                  variant="primary"
+                  className="hidden xl:flex gap-2 items-center"
+                >
+                  <i>
+                    {" "}
+                    <UserCog size={18} className="text-white" />{" "}
+                  </i>
+                  <p className="text-white font-medium "> Acessar Perfil </p>
+                </Button>
+              </div>
+            )}
           </nav>
-          {isLoggedIn !== "true" ? (
-            <div className="flex gap-3">
-              <Button
-                onClick={() => navigate("/login")}
-                size="sm"
-                className="hidden xl:flex gap-2 items-center justify-center "
-                variant="outline"
-              >
-                {" "}
-                <i>
-                  <LogIn size={18} />{" "}
-                </i>{" "}
-                Entrar{" "}
-              </Button>
-              <Button
-                onClick={() => navigate("/registrar")}
-                size="sm"
-                className="hidden xl:flex gap-2 items-center text-white justify-center"
-                variant="primary"
-              >
-                {" "}
-                <i>
-                  <UserRoundPlus size={18} className="text-white" />{" "}
-                </i>{" "}
-                Criar Conta{" "}
-              </Button>
-            </div>
-          ) : (
-            <div className="flex gap-2">
-              <Button
-              size="sm"
-              onClick={() => {
-                navigate("/home");
-                localStorage.setItem("loggedIn", "false");
-                window.location.reload();
-              }}
-              variant="outline"
-              className="hidden xl:flex gap-2 items-center"
-            >
-              <i>
-                {" "}
-                <LogOut size={18}  />{" "}
-              </i>
-              <p className="font-medium "> Sair</p>
-            </Button>
-              <Button
-              size="sm"
-              onClick={() => navigateProfile()}
-              variant="primary"
-              className="hidden xl:flex gap-2 items-center"
-            >
-              <i>
-                {" "}
-                <UserCog size={18} className="text-white" />{" "}
-              </i>
-              <p className="text-white font-medium "> Acessar Perfil </p>
-            </Button>
-            
-            </div>
-          )}
+
           {/* Mobile menu button */}
           {isMobileMenuOpen === false ? (
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-              className="xl:hidden p-2 rounded-lg text-foreground hover:bg-accent"
+              className="xl:hidden p-2 rounded-lg text- text-foreground hover:bg-accent"
               aria-label="Abrir menu"
             >
               <MenuIcon />
@@ -167,8 +167,8 @@ const Header: React.FC = () => {
                     }}
                     className={`block px-4 py-3 rounded-lg text-base font-medium transition-colors ${
                       location.pathname === item.path
-                        ? "text-foreground hover:text-primary font-semibold"
-                        : "text-foreground hover:bg-accent hover:text-accent-foreground"
+                        ? "text-foreground text-sm hover:text-primary font-semibold"
+                        : "text-foreground text-sm hover:text-primary font-semibold"
                     }`}
                   >
                     {item.name}
@@ -186,7 +186,7 @@ const Header: React.FC = () => {
                         setIsMobileMenuOpen(false);
                         navigate("/login");
                       }}
-                      className="block rounded-lg font-medium transition-colors text-primary"
+                      className="block rounded-lg font-medium text-sm transition-colors text-primary"
                     >
                       Entrar
                     </button>
@@ -200,7 +200,7 @@ const Header: React.FC = () => {
                         setIsMobileMenuOpen(false);
                         navigate("/registrar");
                       }}
-                      className="block rounded-lg font-medium transition-colors text-primary"
+                      className="block rounded-lg font-medium transition-colors text-sm text-primary"
                     >
                       Criar Conta
                     </button>
@@ -208,38 +208,37 @@ const Header: React.FC = () => {
                 </>
               ) : (
                 <div className="flex flex-col justify-center items-start px-3 py-2 gap-5">
-                <div className="flex items-center gap-2">
-                <i>
-                  <UserCog size={20} className="text-blue-700" />
-                </i>
-                <button
-                  onClick={() => {
-                    setIsMobileMenuOpen(false);
-                    navigate(`perfil/${localStorage.getItem("userId")}`);
-                  }}
-                  className="block rounded-lg font-medium transition-colors text-blue-700"
-                >
-                 Acessar Perfil
-                </button>
+                  <div className="flex items-center gap-2">
+                    <i>
+                      <UserCog size={20} className="text-primary" />
+                    </i>
+                    <button
+                      onClick={() => {
+                        setIsMobileMenuOpen(false);
+                        navigate(`perfil/${localStorage.getItem("userId")}`);
+                      }}
+                      className="block rounded-lg font-medium transition-colors text-primary"
+                    >
+                      Acessar Perfil
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <i>
+                      <LogOut size={20} className="text-primary" />
+                    </i>
+                    <button
+                      onClick={() => {
+                        setIsMobileMenuOpen(false);
+                        navigate("/home");
+                        localStorage.setItem("loggedIn", "false");
+                        window.location.reload();
+                      }}
+                      className="block rounded-lg font-medium transition-colors text-primary"
+                    >
+                      Sair
+                    </button>
+                  </div>
                 </div>
-                <div className="flex items-center gap-2">
-                <i>
-                  <LogOut size={20} className="text-blue-700" />
-                </i>
-                <button
-                  onClick={() => {
-                    setIsMobileMenuOpen(false);
-                    navigate("/home");
-                    localStorage.setItem("loggedIn", "false");
-                    window.location.reload();
-                  }}
-                  className="block rounded-lg font-medium transition-colors text-blue-700"
-                >
-                Sair
-                </button>
-                </div>
-                
-              </div>
               )}
             </nav>
           </div>
