2ffc28cbe2fdb06be2871ea536be0504b1925ec9
iagoliziero <iagolizierox14@gmail.com>
2025-11-18 21:21:46 -0300

    feat: add steps form API and integrate steps route and update user authentication to use email

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,3 +1,4 @@
+import Steps from "@/routes/etapas";
 import Faq from "@/routes/faq";
 import Home from "@/routes/home";
 import Layout from "@/routes/layout";
@@ -6,6 +7,7 @@ import NotFound from "@/routes/notFound";
 import Register from "@/routes/registrar";
 import About from "@/routes/sobre";
 import { Route, Routes } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
 const AppRoutes = () => {
   return (
@@ -14,6 +16,10 @@ const AppRoutes = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/sobre" element={<About />} />
         <Route path="/faq" element={<Faq />} />
+         <Route path="/etapas" element={<Steps />} />
+          <Route element={<PrivateRoute />} >
+           {/*  <Route path="/etapas" element={<Steps />} /> */}
+          </Route>
       </Route>
 
       <Route path="*" element={<NotFound />} />\
--- src/routes/etapas/index.tsx
+++ src/routes/etapas/index.tsx
@@ -4,7 +4,6 @@ import { Forward } from "lucide-react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 const Steps = () => {
   const [formData, setFormData] = useState({
     finishedSchool: "",
